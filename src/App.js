452edab31b0964a9e073efaad5c202edde7350bf
Filip <filipstevanovic176@gmail.com>
2020-11-12 20:11:57 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { Route } from 'react-router-dom'
+import { Route, HashRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -8,10 +8,12 @@ import Projects from './pages/Projects'
 function App() {
   return (
     <div className="App">
-      <Route path="/" exact component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/projects" component={Projects} />
+      <HashRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/projects" component={Projects} />
+      </HashRouter>
     </div>
   )
 }
